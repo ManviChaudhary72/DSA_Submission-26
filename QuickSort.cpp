04f1fd86fa73a22715a2501c4651bc973758151d
ManviChaudhary72 <manvi.chaudhary052@gmail.com>
2026-08-24 22:13:36 +0530

    Quick Sort

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -2,23 +2,21 @@
 using namespace std;
 
 int Partition(int arr[], int i, int j) {
-
-    int pivot = arr[i];
-
+ int pivot = arr[i];
     int start = i;
     int end = j;
 
-    while (start < end) {
+    while (start<end) {
 
         while (arr[start] <= pivot) {
             start++;
         }
 
-        while (arr[end] > pivot) {
+        while (arr[end]>pivot) {
             end--;
         }
 
-        if (start < end) {
+        if (start<end) {
             swap(arr[start], arr[end]);
         }
     }
@@ -31,10 +29,9 @@ int Partition(int arr[], int i, int j) {
 
 void quickSort(int arr[], int i, int j) {
 
-    if (i < j) {
+    if (i<j) {
 
         int pivotIndex = Partition(arr, i, j);
-
         quickSort(arr, i, pivotIndex - 1);
         quickSort(arr, pivotIndex + 1, j);
     }
@@ -44,11 +41,8 @@ void quickSort(int arr[], int i, int j) {
 int main() {
 
     int arr[] = {30, 12, 9, 55, 23, 77, 11, 90};
-
     int n = sizeof(arr) / sizeof(int);
-
     quickSort(arr, 0, n - 1);
-
     for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
