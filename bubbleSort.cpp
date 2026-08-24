dfc28e89e7fa8a25376fee820f39154d704ffe72
ManviChaudhary72 <manvi.chaudhary052@gmail.com>
2026-08-24 22:57:56 +0530

    bubbleSort.cpp

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[]={5,4,3,2,1};
+    int arr[]={1,2,3,4,5};
+    int swaps=0;
     int n=sizeof(arr)/sizeof(int);
     for(int i=0;i<n-1;i++){
         for(int j=0;j<n-1-i;j++){
@@ -9,8 +10,10 @@ int main(){
                 int temp=arr[j];
                 arr[j]=arr[j+1];
                 arr[j+1]=temp;
+                swaps++;
             }
         }
+        if(swaps==0)break;
      
     }
     for (int i = 0; i < n; i++) {
